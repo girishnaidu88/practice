f1b73ae76ae17781933c0bdc7ef70e0ee8b22b14
girish-kandra <gkandra@apple.com>
2022-02-10 22:55:37 -0600

    Addign more files

---
+++ src/medium/SystemDesign.java
@@ -0,0 +1,33 @@
+package medium;
+
+public class SystemDesign {
+
+    /*
+
+    Amazon:
+    https://raw.githubusercontent.com/codekarle/system-design/master/system-design-prep-material/architecture-diagrams/Amazon%20System%20Design.png
+
+    Airbin / oyo:
+    https://github.com/codekarle/system-design/blob/master/system-design-prep-material/architecture-diagrams/Hoel%20Booking%20System.png
+
+    whatsapp / FB messenger:
+    https://github.com/codekarle/system-design/blob/master/system-design-prep-material/architecture-diagrams/Whatsapp%20System%20design.png
+
+    uber/ lyft/ ola/ taxi:
+    https://github.com/codekarle/system-design/blob/master/system-design-prep-material/architecture-diagrams/Uber%20System%20Design.png
+
+    facebook:
+    https://github.com/codekarle/system-design/blob/master/system-design-prep-material/architecture-diagrams/Facebook%20System%20Design.png
+
+    Twitter:
+    https://github.com/codekarle/system-design/blob/master/system-design-prep-material/architecture-diagrams/Twitter%20System%20Design.png
+
+    Netflix/ prime:
+    https://github.com/codekarle/system-design/blob/master/system-design-prep-material/architecture-diagrams/Video%20Streaming%20Platform.png
+
+
+
+
+     */
+
+}
